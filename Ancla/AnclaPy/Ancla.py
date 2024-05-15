c2802eec77c33d7c82752cb9c988650696eece24
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-05-15 16:11:26 -0500

    command line args working and more viz functions

--- Ancla/AnclaPy/Ancla.py
+++ Ancla/AnclaPy/Ancla.py
@@ -1,6 +1,10 @@
+import stat
+from statistics import LinearRegression
 from numpy._typing import NDArray
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class HelperFunctions(object):
     
@@ -15,7 +19,6 @@ class HelperFunctions(object):
         data.to_csv(path, index=False)
 
 class Visualize(object):
-    import matplotlib.pyplot as plt
     
     @staticmethod
     def scatter_plots(data: pd.DataFrame) -> None:
@@ -29,16 +32,21 @@ class Visualize(object):
         from sklearn.metrics import r2_score
 
         #first plot
-        axs[0].scatter(data['Database'], data['Database'], color='red', label='Database vs Database', s=0.3)
-        axs[0].scatter(data['Experimental'], data['Database'], color='blue', label='Experimental vs Database', s=0.3)
+        axs[0].scatter(data['Database'], data['Database'], 
+                       color='red', label='Database vs Database', s=0.3)
+        axs[0].scatter(data['Experimental'], data['Database'], 
+                       color='blue', label='Experimental vs Database', s=0.3)
         axs[0].set_xlabel('Experimental')
         axs[0].set_ylabel('Database')
         axs[0].legend()
         
         #second plot
-        axs[1].scatter(data['Database'], data['Database'], color='red', label='Database vs Database', s=0.3)
-        axs[1].scatter(data['Experimental'], data['Database'], color='blue', label='Experimental vs Database', s=0.3)
-        axs[1].scatter(data['PostTransformation'], data['Database'], color='green', label='PostTransformation vs Database', s=0.3)
+        axs[1].scatter(data['Database'], data['Database'],
+                       color='red', label='Database vs Database', s=0.3)
+        axs[1].scatter(data['Experimental'], data['Database'],
+                       color='blue', label='Experimental vs Database', s=0.3)
+        axs[1].scatter(data['PostTransformation'], data['Database'],
+                       color='green', label='PostTransformation vs Database', s=0.3)
         axs[1].set_xlabel('PostTransformation')
         axs[1].set_ylabel('Database')
         axs[1].legend()
@@ -49,7 +57,110 @@ class Visualize(object):
 
         r2 = r2_score(data['Database'], data['PostTransformation'])
         axs[1].text(0.5, 0.9, f'R2: {r2:.2f}', transform=axs[1].transAxes)
+        
+    @staticmethod
+    def z_scores_hist(data: pd.DataFrame) -> None:
+        from scipy.stats import zscore
+        
+        #compute the z-scores for the Database and Experimental columns, then database and PostTransformation
+        data['Database_z'] = zscore(data['Database'])
+        data['Experimental_z'] = zscore(data['Experimental'])
+        data['PostTransformation_z'] = zscore(data['PostTransformation'])
+        
+        #show the z-scores before and after the transformation. I want to see if the distribution is the same
+        fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+        
+        axs[0].hist(data['Database_z'], bins=100, color='red', alpha=0.5, label='Database')
+        axs[0].hist(data['Experimental_z'], bins=100, color='blue', alpha=0.5, label='Experimental')
+        axs[0].set_title('Before transformation')
+        axs[0].set_xlabel('Z-score')
+        axs[0].set_ylabel('Frequency')
+        axs[0].legend()
+        
+        axs[1].hist(data['Database_z'], bins=100, color='red', alpha=0.5, label='Database')
+        axs[1].hist(data['PostTransformation_z'], bins=100, color='green', alpha=0.5, label='PostTransformation')
+        axs[1].set_title('After transformation')
+        axs[1].set_xlabel('Z-score')
+        axs[1].set_ylabel('Frequency')
+        axs[1].legend()
+        
+        #add trendline to the histograms
+        from scipy.stats import norm
+        from scipy import stats
+       
+        #fit a normal distribution to the data
+        mu, std = norm.fit(data['Database_z'])
+        x = np.linspace(-5, 5, 100)
+        p = norm.pdf(x, mu, std)
+        
+        axs[0].plot(x, p, 'k', linewidth=2)
+        axs[1].plot(x, p, 'k', linewidth=2)
 
+    @staticmethod
+    def residuals_scatter(data: pd.DataFrame) -> None:
+        #compute the residuals for the experimental and post transformation columns
+        data['Residuals_exp'] = data['Database'] - data['Experimental']
+        data["Residuals_trans"] = data['Database'] - data['PostTransformation']
+        
+        #plot the data and the residuals in two plots in the same grid 
+        fig, axs = plt.subplots(2, 2, figsize=(15, 5))
+        
+        axs[0, 0].scatter(data['Database'], data['Database'],
+                          color='red', label='Database vs Database', s=0.3)
+        axs[0, 0].scatter(data['Experimental'], data['Database'],
+                          color='blue', label='Experimental vs Database', s=0.3)
+        axs[0, 0].set_xlabel('Experimental')
+        axs[0, 0].set_ylabel('Database')
+        axs[0, 0].legend()
+        
+        axs[0, 1].scatter(range(len(data["Residuals_exp"])), data['Residuals_exp'], 
+                          color='blue', label='Residuals Experimental', s=0.3)
+        #horizontal black line at 0
+        axs[0, 1].axhline(0, color='black', lw=1)
+        axs[0, 1].set_xlabel('Residuals Experimental')
+        axs[0, 1].set_ylabel('Database')
+        axs[0, 1].legend()
+        
+        axs[1, 0].scatter(data['Database'], data['Database'],
+                          color='red', label='Database vs Database', s=0.3)
+        axs[1, 0].scatter(data['PostTransformation'], data['Database'],
+                          color='green', label='PostTransformation vs Database', s=0.3)
+        axs[1, 0].set_xlabel('PostTransformation')
+        axs[1, 0].set_ylabel('Database')
+        axs[1, 0].legend()
+        
+        axs[1, 1].scatter(range(len(data["Residuals_trans"])), data["Residuals_trans"],
+                          color='green', label='Residuals PostTransformation', s=0.3)
+        #horizontal black line at 0
+        axs[1, 1].axhline(0, color='black', lw=1)
+        axs[1, 1].set_xlabel('Residuals PostTransformation')
+        axs[1, 1].set_ylabel('Database')
+        axs[1, 1].legend()
+        
+
+    @staticmethod
+    def qq_plot(data: pd.DataFrame) -> None:
+        from scipy.stats import probplot
+        
+        #compute the residuals for the experimental and post transformation columns
+        data['Residuals_exp'] = data['Database'] - data['Experimental']
+        data["Residuals_trans"] = data['Database'] - data['PostTransformation']
+        
+        #plot the qq plot for the residuals
+        fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+        
+        probplot(data['Residuals_exp'], plot=axs[0])
+        axs[0].set_title('QQ plot for the residuals of the Experimental column')
+        
+        probplot(data['Residuals_trans'], plot=axs[1])
+        axs[1].set_title('QQ plot for the residuals of the PostTransformation column')
+
+    @staticmethod
+    def visualize_all(data: pd.DataFrame) -> None:
+        Visualize.scatter_plots(data)
+        Visualize.z_scores_hist(data)
+        Visualize.residuals_scatter(data)
+        Visualize.qq_plot(data)
         plt.show()
 
 class RegressiveModels(object):
@@ -91,9 +202,11 @@ class TransformationFunctions(object):
         data['PostTransformation'] = np.interp(data['Experimental'], lowess_model[:, 0], lowess_model[:, 1])
     
         return data
-
-if __name__ == '__main__':
-    data = read_results(r'D:\transformedData_RAW.csv')
-    model = fit_lowess(data)
-    data = transform_experimental(data, model)
-    scatter_plots(data)
+    
+    @staticmethod
+    def transform_experimental_linear(data: pd.DataFrame, linear_model: LinearRegression,
+                               x: str="Experimental", y: str="Database") -> pd.DataFrame:
+        #transform the experimental values
+        data['PostTransformation'] = linear_model.predict(data[x].values.reshape(-1, 1))
+    
+        return data
---
+++ Ancla/AnclaPy/run_ancla.py
@@ -0,0 +1,38 @@
+import sys
+import argparse
+from Ancla import HelperFunctions, Visualize, RegressiveModels, TransformationFunctions
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    #parse the arguments
+    
+    parser = argparse.ArgumentParser(description='AnclaPy: :) ')
+    
+    parser.add_argument('--input', type=str, help='Input file path')
+    parser.add_argument('--output', type=str, help='Output file path')
+    parser.add_argument('--reg', type=str, default='linear',
+                        help='Type of regression to use (linear or lowess)')
+    parser.add_argument('--viz', type=bool, default=True,
+                        help = "Visualize the results")
+    
+    args = parser.parse_args()
+
+    #read the data
+    data = HelperFunctions.read_results(args.input)
+   
+    #fit and transform the data
+    if args.reg == 'linear':
+        model = RegressiveModels.fit_linear(data)
+        data = TransformationFunctions.transform_experimental_linear(data, model)
+    elif args.reg == 'lowess':
+        model = RegressiveModels.fit_lowess(data)
+        data = TransformationFunctions.transform_experimental_lowess(data, model)
+    else:
+        sys.exit("Invalid regressor")
+    
+    #save the data
+    data.to_csv(args.output)
+    
+    #visualize the data
+    if args.viz:
+        Visualize.visualize_all(data)
