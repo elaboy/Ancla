b244dff32b99c2199d245e70debb549d15773656
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-07-08 16:44:42 -0500

    updated the dictionaries

--- Ancla/AnchorCommandLine/Program.cs
+++ Ancla/AnchorCommandLine/Program.cs
@@ -1,10 +1,9 @@
 ﻿using Database;
-using Easy.Common.Interfaces;
+using Giraffe.ViewEngine;
 using MathNet.Numerics.Statistics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.ML;
 using Microsoft.ML.Data;
-using mzIdentML110.Generated;
 using Proteomics.PSM;
 using Readers;
 
@@ -36,7 +35,7 @@ public class Calibrator
     public Dictionary<string, double> LibraryRetentionTimes = new Dictionary<string, double>();
     public List<FileLogger> FileLoggers = new List<FileLogger>();
     public Calibrator(string filePath)
-    { 
+    {
         // read the psmtsv file
         var psmtsv = new PsmFromTsvFile(filePath);
         psmtsv.LoadResults();
@@ -52,6 +51,28 @@ public class Calibrator
             fileLogger.Calibrate();
         }
     }
+
+    //this method should return a list of dictionaries where the key is the full sequence and the value is the retention time
+    public Dictionary<string, Dictionary<string, double>> GetCalibrations()
+    {
+        Dictionary<string, Dictionary<string, double>> calibrations = new Dictionary<string, Dictionary<string, double>>();
+
+        foreach (var rawFile in FileLoggers[0].FollowingRawFiles)
+        {
+            Dictionary<string, double> rawFileSequences = new Dictionary<string, double>();
+
+            foreach (var fullSequence in rawFile.FullSequenceWithScanRetentionTime)
+            {
+                rawFileSequences.Add(fullSequence.Key, fullSequence.Value);
+            }
+            //sort by value
+            rawFileSequences = rawFileSequences.OrderBy(p => p.Value).ToDictionary(p => p.Key, p => p.Value);
+
+            calibrations.Add(rawFile.RawFileName, rawFileSequences);
+        }
+
+        return calibrations;
+    }
 }
 
 public class FileLogger
@@ -62,27 +83,28 @@ public class FileLogger
     public List<RawFileLogger> FollowingRawFiles = new();
     public Dictionary<string, RawFileLogger> RawFiles = new Dictionary<string, RawFileLogger>();
     public Dictionary<string, List<(string, double?)>> FullSequencesPresentInFile = new Dictionary<string, List<(string, double?)>>();
+    public Dictionary<string, List<(string, double)>> FileWiseCalibrations = new Dictionary<string, List<(string, double)>>();
     public FileLogger(PsmFromTsvFile file)
     {
         File = file;
         FilePath = file.FilePath;
 
         var rawFiles = file.Results.GroupBy(p => p.FileNameWithoutExtension);
-        
+
         // sorts the psms by raw file name, where the RawFiles dictionary key is the raw file name and the value is a RawFileLogger object 
         foreach (var rawFileName in rawFiles)
         {
             RawFiles.Add(rawFileName.Key, new RawFileLogger(rawFileName.Key, rawFileName.Select(p => p)));
         }
-        
+
         // Get a list of all the full sequences present in the file
         List<string> fullSequences = file.Results.Select(p => p.FullSequence)
                                                  .Distinct()
                                                  .ToList();
-        
+
         // Retention Times will be initialized as null
         FullSequencesPresentInFile =
-            fullSequences.ToDictionary(p => p, p => new List<(string, double?)>() { ("Init", (double?)null) });
+            fullSequences.ToDictionary(p => p, p => new List<(string, double?)>());
 
         // pick the leading raw file and set the follower raw files
         LeadingRawFile = RawFiles.Values.OrderBy(r => r.Psms.Count()).First();
@@ -99,41 +121,30 @@ public class FileLogger
 
     private void PairwiseCalibration(RawFileLogger followingRawFile)
     {
-        // Order the full sequences by retention time
-        var orderedLeaderFullSequences = LeadingRawFile.FullSequenceWithScanRetentionTime
-            .OrderBy(p => p.Value).ToList();
-
-        var orderedFollowerFullSequences = followingRawFile.FullSequenceWithScanRetentionTime
-            .OrderBy(p => p.Value).ToList();
-
         // Get overlapping peptides between the leading and following raw file
-        var overlappingPeptides = orderedLeaderFullSequences.Select(p => p.Key)
-            .Intersect(orderedFollowerFullSequences.Select(p => p.Key)).ToList();
+        var overlappingFullSequences = LeadingRawFile.FullSequenceWithScanRetentionTime.Keys
+            .Intersect(followingRawFile.FullSequenceWithScanRetentionTime.Keys);
 
-        // get these full sequences from the leader and follower raw file as an array of doubles
-        var leaderRetentionTimes = overlappingPeptides
-            .Select(p => (orderedLeaderFullSequences.First(x => x.Key.Equals(p)))).ToList();
+        Dictionary<string, (double, double)> overlappingPsms = new();
 
-        var followerRetentionTimes = overlappingPeptides
-            .Select(p => (orderedFollowerFullSequences.First(x => x.Key.Equals(p)))).ToList();
-
-        // assert that the leader and follower retention times are the same length
-        if (leaderRetentionTimes.Count != followerRetentionTimes.Count)
+        foreach (var sequence in overlappingFullSequences)
         {
-            throw new Exception("Leader and follower anchors retention times are not the same length");
+            overlappingPsms.Add(sequence, (LeadingRawFile.FullSequenceWithScanRetentionTime[sequence],
+                followingRawFile.FullSequenceWithScanRetentionTime[sequence]));
         }
 
         // use ml.net to train a linear regression model using the leader and follower retention times as training data
         MLContext mlContext = new MLContext();
         var data = new List<Anchor>();
 
-        for (int i = 0; i < leaderRetentionTimes.Count; i++)
+
+        foreach (var overlappingPsm in overlappingPsms)
         {
             data.Add(new Anchor
             {
-                FullSequence = overlappingPeptides[i],
-                LeaderRetentionTime = (float)leaderRetentionTimes[i].Value,
-                FollowerRetentionTime = (float)followerRetentionTimes[i].Value
+                FullSequence = overlappingPsm.Key,
+                LeaderRetentionTime = (float)overlappingPsm.Value.Item1,
+                FollowerRetentionTime = (float)overlappingPsm.Value.Item2
             });
         }
 
@@ -157,8 +168,26 @@ public class FileLogger
                 FollowerRetentionTime = (float)fullSequence.Value
             });
 
+            if(!FileWiseCalibrations.ContainsKey(fullSequence.Key))
+            {
+                FileWiseCalibrations.Add(fullSequence.Key, new List<(string, double)>());
+            }
+
             // update the retention time of the full sequence in the following raw file
-            FullSequencesPresentInFile[fullSequence.Key].Add((followingRawFile.RawFileName, prediction.TransformedRetentionTime));
+            FileWiseCalibrations[fullSequence.Key].Add((followingRawFile.RawFileName + "_Calibrated",
+                prediction.TransformedRetentionTime));
+        }
+
+        // for each full sequence in the leading raw file, insert those that are not present in the following raw file
+
+        foreach (var fullSequence in LeadingRawFile.FullSequenceWithScanRetentionTime)
+        {
+            if (!FileWiseCalibrations.ContainsKey(fullSequence.Key))
+            {
+                FileWiseCalibrations.Add(fullSequence.Key, new List<(string, double)>());
+            }
+
+            FileWiseCalibrations[fullSequence.Key].Add((LeadingRawFile.RawFileName + "_OG", fullSequence.Value));
         }
     }
 }
@@ -185,9 +214,9 @@ public class RawFileLogger
     {
         RawFileName = rawFileName;
         // TODO Filter the psms
-        Psms = psms.Where(p => p.QValue <= 0.01 & 
-                               p.PEP <= 0.5 & 
-                               p.AmbiguityLevel == "1" & 
+        Psms = psms.Where(p => p.QValue <= 0.01 &
+                               p.PEP <= 0.5 &
+                               p.AmbiguityLevel == "1" &
                                p.DecoyContamTarget == "T").ToList();
 
         // Get the median retention time for each full sequence that are repeated in the raw file
